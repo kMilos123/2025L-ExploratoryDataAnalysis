f8603ede1dccfa0b079f1c7b9d662dc2caffbec0
kMilos123 <k.twarowski@interia.pl>
2025-02-25 10:03:43 +0100

    Add files via upload

---
+++ homeworks/KamilTwarowski/proton.R
@@ -0,0 +1,9 @@
+install.packages("proton")
+library(proton)
+proton()
+
+employees
+proton(action = "login", login="XYZ")
+
+login <- employees[employees$name == "John" & 
+            employees$surname == "Insecure","login"]
